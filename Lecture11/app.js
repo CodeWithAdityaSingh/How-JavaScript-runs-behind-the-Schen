8a03dc282795ca126f5769ea753e7a5ca090db7c
Aditya Singh <adityasingh161020003@gmail.com>
2024-06-11 21:30:21 +0530

    Create app.js

---
+++ Lecture11/app.js
@@ -0,0 +1,84 @@
+// // setTimeout(()=>{
+// //     let counter = 0
+// //     counter++
+// //     counter++
+// //     counter++
+// //     console.log(counter);
+    
+// // },1000)
+
+// let b = 200
+
+// function a(){
+//   var a = 200
+
+//     function b(){
+//         console.log('b');
+        
+//     }
+//     b()
+    
+// }
+
+// a()
+
+// {
+//     let a = 200
+//     {
+//         let b = 400
+//         {
+//             let c = 500
+//         }
+//     }
+// }
+
+
+
+// let b = 200
+// function a(){
+
+//     console.log(b);
+    
+// }
+// a()
+
+
+
+// console.log(b);
+// function a(){
+//     let a = 200
+//     let b = 500
+//     console.log(a,b);
+//     return a
+    
+// }
+
+// let b = a()
+// console.log(b);
+
+
+// function addition(){
+
+//     let s = 0
+
+//     for(let i=0 ; i <=10 ; i++){
+
+//         s  += i
+//         console.log(s);
+
+        
+//     }
+
+//     return s
+// }
+
+// let s = addition()
+// console.log(s);
+function a(){
+
+    var a = 3
+}
+
+a()
+
+
